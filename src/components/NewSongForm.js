a0fe72bab4836b904b10d1765d8c068de966e2cd
Rupam <rupam.shaw@gmail.com>
2017-10-18 11:21:29 +1100

    adding title to /songs by callback addSong and it was already storing in Db

--- src/components/NewSongForm.js
+++ src/components/NewSongForm.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react'
 
+const newSongStyles = {
+    padding: '10px'
+}
+  
 class NewSongForm extends Component {
+    constructor(props) {
+      super(props)
+      this.createSong = this.createSong.bind(this)
+    }
+  
+    createSong(event) {
+      event.preventDefault()
+      const title = this.titleInput.value
+      this.props.addSong(title) //callback of parent
+      this.songForm.reset()
+      this.props.postSubmitHandler()
+    }
+  
     render() {
-        return(
-            <div >
-                <h1>Hello</h1>
-            </div>
-        )
+      return (
+        <div style={newSongStyles}>
+          <form onSubmit={(event) => this.createSong(event)} ref={(form) => this.songForm = form}>
+            <label className="pt-label">
+              Song Title
+              <input style={{width: "100%"}} className="pt-input" name="title" type="text" ref={(input) => { this.titleInput = input }} placeholder="Don't Stop Believing"></input>
+            </label>
+            <input style={{width: "100%"}} type="submit" className="pt-button pt-intent-primary" value="Create Song"></input>
+          </form>
+        </div>
+      )
     }
 }
 
